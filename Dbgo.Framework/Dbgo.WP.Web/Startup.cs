ef257ab5b2fa7992df03432de348ea4520e4c946
a271853837 <lichuanlu521@126.com>
2017-12-01 23:22:34 +0800

    添加污普项目
    污普项目demo

---
+++ Dbgo.Framework/Dbgo.WP.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dbgo.WP.Web.Startup))]
+namespace Dbgo.WP.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
