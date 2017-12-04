bfd4ca73b31534d64b1082abb8b9c6e6459fd29c
a271853837 <lichuanlu521@126.com>
2017-12-04 23:25:45 +0800

    修改
    修改

--- Dbgo.Framework/CommonTest/Program.cs
+++ Dbgo.Framework/CommonTest/Program.cs
@@ -11,23 +11,41 @@ namespace CommonTest
     {
         static void Main(string[] args)
         {
-            SyncHelper sync = new SyncHelper();
-            
 
-            Thread thread = new Thread(delegate()
+
+            //SyncHelper sync = new SyncHelper();
+
+
+            //Thread thread = new Thread(delegate()
+            //{
+            //    SyncHelper.LockType();
+            //});
+            //thread.Start();
+
+            //#region 1
+            //Timer time = new Timer(delegate
+            //    {
+            //        SyncHelper.Excute();
+            //    }, null, 0, 1000);
+            //#endregion
+
+            List<int> list = new List<int>();
+            for (int i = 0; i < 31; i++)
             {
-                SyncHelper.LockType();
-            });
-            thread.Start();
-
-            #region 1
-            Timer time = new Timer(delegate
-                {
-                    SyncHelper.Excute();
-                }, null, 0, 1000);
-            #endregion
-
+                R r = new R();
+                int num = r.CreatNum(202200, 1002000);
+                list.Add(num);
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in list)
+            {
+                sb.Append(item + ",");
+            }
+            Console.WriteLine(sb);
             Console.Read();
         }
+
+
+        
     }
 }
---
+++ Dbgo.Framework/CommonTest/R.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTest
+{
+    public class R
+    {
+        public int CreatNum(int min, int max)
+        {
+            byte[] buffer = Guid.NewGuid().ToByteArray();
+            int iSeed = BitConverter.ToInt32(buffer, 0);
+            Random ran = new Random(iSeed);
+            int RandKey = ran.Next(min, max);
+            return RandKey;
+        }
+        
+    }
+}
